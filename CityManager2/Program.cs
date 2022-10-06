44d549f7cc7075e30af5e5f6cabb166152dc228a
João Inácio <egnawake@gmail.com>
2022-10-06 22:40:50 +0100

    Create CityManager2 project

---
+++ CityManager2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CityManager2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
