ba6cea2bf37db3243fd54d237be79bf5ca39498c
João Inácio <egnawake@gmail.com>
2022-10-13 00:36:01 +0100

    Init and fill podiums

--- Podium/Program.cs
+++ Podium/Program.cs
@@ -6,7 +6,14 @@ namespace Podium
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Podium<Building> buildingPodium = new Podium<Building>(3);
+            buildingPodium.Add(new Building("Building1", 1000f, 200f));
+            buildingPodium.Add(new Building("Building2", 800f, 500f));
+            buildingPodium.Add(new Building("Building3", 460f, 800f));
+            buildingPodium.Add(new Building("Building4", 20000f, 3000f));
+            buildingPodium.Add(new Building("Building5", 1350f, 675f));
+
+            Podium<int> intPodium = new Podium<int>(5) { 8, 4, 17, 23, 2, 5, 11 };
         }
     }
 }
